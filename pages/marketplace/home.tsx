e70a5368eb02ff8f05bcf45254b24af94ce78a48
danielsmythy <100067118+danielsmythy@users.noreply.github.com>
2022-03-12 21:08:24 +1100

    Bug fix.

--- pages/marketplace/home.tsx
+++ pages/marketplace/home.tsx
@@ -1,10 +1,9 @@
 import React, { useEffect } from "react";
-import { loadMarketplace } from "../../lib/thirdweb/MarketPlace";
 
 function Home() {
   useEffect(() => {
     const load = async () => {
-      loadMarketplace();
+      // loadMarketplace();
     };
     load();
   });
