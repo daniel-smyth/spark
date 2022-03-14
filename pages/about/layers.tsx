ad85481702e0cc85616dda979ee39a4127b2abc5
danielsmythy <100067118+danielsmythy@users.noreply.github.com>
2022-03-14 15:38:00 +1100

    Layers completed.

--- pages/about/layers.tsx
+++ pages/about/layers.tsx
@@ -9,6 +9,9 @@ import {
   Icon,
   useColorModeValue,
   Image,
+  Button,
+  Link,
+  Box,
 } from "@chakra-ui/react";
 import { ReactElement } from "react";
 import { FiDatabase, FiFolder, FiUpload } from "react-icons/fi";
@@ -16,9 +19,9 @@ import Script from "next/script";
 
 export default function LayersInfo() {
   return (
-    <Container maxW="6xl" spacing={{ base: 8, md: 8 }}>
+    <Container maxW="6xl" py={16} spacing={{ base: 8, md: 8 }}>
       <SimpleGrid
-        columns={{ base: 1, md: 2 }}
+        columns={{ base: 1, md: 1 }}
         spacing={10}
         px={{ base: 5 }}
         pb={{ base: 16, md: 36 }}
@@ -27,8 +30,9 @@ export default function LayersInfo() {
           <Heading fontSize={{ base: "2xl", md: "3xl" }}>
             What are layers?
           </Heading>
+
           <Stack
-            spacing={3}
+            spacing={4}
             divider={
               <StackDivider
                 borderColor={useColorModeValue("gray.100", "gray.700")}
@@ -40,13 +44,37 @@ export default function LayersInfo() {
               iconBg={"#3199FF"}
               text={"1. Layers are traits"}
             />
-            <Text size="sm">
-              Think of layers as folders which contain your artwork's traits.
-              The layer name you provider will be the trait name in the NFT. For
-              example, BoredApes have a trait (or layer) called "hat". The hat
-              then has different traits, or images, with names like "Trippy
-              Captain's Hat" and "King's Crown."
-            </Text>
+            <Flex>
+              <Box display={{ base: "none", md: "block" }} py={2} px={8}>
+                <Image
+                  shadow={"lg"}
+                  alignSelf={"center"}
+                  width={"320px"}
+                  h={"137px"}
+                  src="/layersScreenshot.jpg"
+                ></Image>
+              </Box>
+              <Text>
+                Layers are like{" "}
+                <span style={{ fontStyle: "italic" }}>folders</span> containing
+                artwork <span style={{ fontStyle: "italic" }}>traits</span>. A
+                collection can have many{" "}
+                <span style={{ fontStyle: "italic" }}>layers</span> to increase
+                collection size and style.
+                <br />
+                <br />
+                BoredApes have a{" "}
+                <span style={{ fontStyle: "italic" }}>layer</span> (or{" "}
+                <span style={{ fontStyle: "italic" }}>trait</span>), "hat", with
+                variations like "Trippy Captain's Hat" and "King's Crown."
+                <br />
+                <br />
+                Left there are two{" "}
+                <span style={{ fontStyle: "italic" }}>layers</span> /{" "}
+                <span style={{ fontStyle: "italic" }}>traits</span>. Background
+                and hat.
+              </Text>
+            </Flex>
           </Stack>
           <Stack
             spacing={3}
@@ -61,16 +89,20 @@ export default function LayersInfo() {
               iconBg={"#3199FF"}
               text={"2. How do I upload layers?"}
             />
-            <Stack spacing={3}>
+            <Stack py={2} px={{ base: "none", md: 8 }} spacing={5}>
               <Text>
-                When you are uploading images to Spark3 it will detect the layer
-                name from the name of the image. We handle the rest.
+                When uploading images Spark3 will detect the layer name from the
+                image. We handle the rest including NFT metadata..
               </Text>
               <Text variant={"bold"} size="md">
                 "LAYERNAME_IMAGENAME.png"
               </Text>
-              <Text size="md">"Background_blue.png"</Text>
-              <Text size="md">"Hat_redFedora.png"</Text>
+              <Text pt={1} size={"md"}>
+                Ready to start?{" "}
+                <Link color={"blue.400"} href="/createcollection/create">
+                  Upload layers
+                </Link>
+              </Text>
             </Stack>
           </Stack>
           <Stack
@@ -88,22 +120,29 @@ export default function LayersInfo() {
               iconBg={"#3199FF"}
               text={"3. Layer names are add as traits to NFT metadata"}
             />
-            <Text size="sm">
-              Layer names are detected and added to Metadata to display on
-              exchanges like OpenSea.
-            </Text>
-            <Image src="/layers.jpg"></Image>
+            <SimpleGrid
+              columns={{ base: 1, md: 2 }}
+              py={2}
+              px={{ base: 0, md: 8 }}
+              spacing={6}
+            >
+              <Text size="sm">
+                Layer names are added to the NFT Metadata and displayed on
+                exchanges like OpenSea. Each layer then has a certain number of
+                unique traits, which are more specific, and each unique trait
+                would appear a certain number of times in the NFT collection.
+              </Text>
+              <Flex
+                display={{ base: "none", md: "block" }}
+                py={2}
+                px={8}
+                minW={"500px"}
+                alignSelf={"center"}
+              >
+                <Image shadow={"md"} src="/layers.jpg"></Image>
+              </Flex>
+            </SimpleGrid>
           </Stack>
-        </Stack>
-        <Stack>
-          <Text size="sm">
-            Create an NFT collection in minutes thanks to Spark3's randomising
-            art engine and the latest advancements in Web3 technology.
-          </Text>
-          <Text>
-            Calculate your required layers and traits above and you are ready to
-            start
-          </Text>
         </Stack>
       </SimpleGrid>
     </Container>
--- pages/about/traits.tsx
+++ pages/about/traits.tsx
@@ -8,16 +8,20 @@ import {
   StackDivider,
   Icon,
   useColorModeValue,
+  Image,
+  Button,
+  Link,
   Box,
 } from "@chakra-ui/react";
 import { ReactElement } from "react";
-import { FiImage, FiDatabase, FiRepeat } from "react-icons/fi";
+import { FiDatabase, FiFolder, FiUpload } from "react-icons/fi";
+import Script from "next/script";
 
 export default function TraitsInfo() {
   return (
-    <Container maxW="6xl" spacing={{ base: 8, md: 8 }}>
+    <Container maxW="6xl" py={16} spacing={{ base: 8, md: 8 }}>
       <SimpleGrid
-        columns={{ base: 1, md: 2 }}
+        columns={{ base: 1, md: 1 }}
         spacing={10}
         px={{ base: 5 }}
         pb={{ base: 16, md: 36 }}
@@ -26,8 +30,9 @@ export default function TraitsInfo() {
           <Heading fontSize={{ base: "2xl", md: "3xl" }}>
             What are layers?
           </Heading>
+
           <Stack
-            spacing={3}
+            spacing={4}
             divider={
               <StackDivider
                 borderColor={useColorModeValue("gray.100", "gray.700")}
@@ -35,17 +40,41 @@ export default function TraitsInfo() {
             }
           >
             <Feature
-              icon={<Icon as={FiImage} color={"#1B486D"} w={5} h={5} />}
+              icon={<Icon as={FiFolder} color={"white"} w={5} h={5} />}
               iconBg={"#3199FF"}
-              text={"1. Layers contain artwork traits"}
+              text={"1. Layers are traits"}
             />
-            <Text size="sm">
-              Layers are folders which contain the images for your artwork. The
-              name for of you layer will be the name of the trait for the
-              artwork. For example, BoredApes have a trait (or layer) called
-              "hat". The hat then has different traits, or images, with names
-              like "Trippy Captain's Hat" and "King's Crown."
-            </Text>
+            <Flex>
+              <Box display={{ base: "none", md: "block" }} py={2} px={8}>
+                <Image
+                  shadow={"lg"}
+                  alignSelf={"center"}
+                  width={"320px"}
+                  h={"137px"}
+                  src="/layersScreenshot.jpg"
+                ></Image>
+              </Box>
+              <Text>
+                Layers are like{" "}
+                <span style={{ fontStyle: "italic" }}>folders</span> containing
+                artwork <span style={{ fontStyle: "italic" }}>traits</span>. A
+                collection can have many{" "}
+                <span style={{ fontStyle: "italic" }}>layers</span> to increase
+                collection size and style.
+                <br />
+                <br />
+                BoredApes have a{" "}
+                <span style={{ fontStyle: "italic" }}>layer</span> (or{" "}
+                <span style={{ fontStyle: "italic" }}>trait</span>), "hat", with
+                variations like "Trippy Captain's Hat" and "King's Crown."
+                <br />
+                <br />
+                Left there are two{" "}
+                <span style={{ fontStyle: "italic" }}>layers</span> /{" "}
+                <span style={{ fontStyle: "italic" }}>traits</span>. Background
+                and hat.
+              </Text>
+            </Flex>
           </Stack>
           <Stack
             spacing={3}
@@ -56,22 +85,25 @@ export default function TraitsInfo() {
             }
           >
             <Feature
-              icon={<Icon as={FiDatabase} color={"#1B486D"} w={5} h={5} />}
+              icon={<Icon as={FiUpload} color={"white"} w={5} h={5} />}
               iconBg={"#3199FF"}
-              text={"2. Spark3 detects layer names from the image name"}
+              text={"2. How do I upload layers?"}
             />
-            <Text>
-              When you are uploading images to Spark3 it will detect the layer
-              name from the name of the image. We handle the rest.
-            </Text>
-          </Stack>
-          <Stack spacing={6}>
-            <Text variant={"bold"} size="md">
-              "LAYERNAME_IMAGENAME.png"
-            </Text>
-            <Text size="md">
-              "Background_blue.png"{<br />}"Hat_redFedora.png"
-            </Text>
+            <Stack py={2} px={{ base: "none", md: 8 }} spacing={5}>
+              <Text>
+                When uploading images Spark3 will detect the layer name from the
+                image. We handle the rest including NFT metadata..
+              </Text>
+              <Text variant={"bold"} size="md">
+                "LAYERNAME_IMAGENAME.png"
+              </Text>
+              <Text pt={1} size={"md"}>
+                Ready to start?{" "}
+                <Link color={"blue.400"} href="/createcollection/create">
+                  Upload layers
+                </Link>
+              </Text>
+            </Stack>
           </Stack>
           <Stack
             spacing={3}
@@ -83,26 +115,34 @@ export default function TraitsInfo() {
           >
             <Feature
               icon={
-                <Icon as={FiRepeat} color={"#1B486D"} w={5} h={5} pb={0.5} />
+                <Icon as={FiDatabase} color={"white"} w={5} h={5} pb={0.5} />
               }
               iconBg={"#3199FF"}
-              text={"3. Spark3 adds layers as traits to NFT metadata"}
+              text={"3. Layer names are add as traits to NFT metadata"}
             />
-            <Text size="sm">
-              Layer names are detected and added to Metadata to display on
-              exchanges like OpenSea.
-            </Text>
+            <SimpleGrid
+              columns={{ base: 1, md: 2 }}
+              py={2}
+              px={{ base: 0, md: 8 }}
+              spacing={6}
+            >
+              <Text size="sm">
+                Layer names are added to the NFT Metadata and displayed on
+                exchanges like OpenSea. Each layer then has a certain number of
+                unique traits, which are more specific, and each unique trait
+                would appear a certain number of times in the NFT collection.
+              </Text>
+              <Flex
+                display={{ base: "none", md: "block" }}
+                py={2}
+                px={8}
+                minW={"500px"}
+                alignSelf={"center"}
+              >
+                <Image shadow={"md"} src="/layers.jpg"></Image>
+              </Flex>
+            </SimpleGrid>
           </Stack>
-        </Stack>
-        <Stack>
-          <Text size="sm">
-            Create an NFT collection in minutes thanks to Spark3's randomising
-            art engine and the latest advancements in Web3 technology.
-          </Text>
-          <Text>
-            Calculate your required layers and traits above and you are ready to
-            start
-          </Text>
         </Stack>
       </SimpleGrid>
     </Container>
